7dfa529a3079bdd007a00492a884057863930a0f
Graham Markall <graham@opengamma.com>
2013-09-24 08:27:30 +0100

    [MAT-221] Fix confusing PtrVector class hierarchy
    PtrVector is now the bases. OwningPtrVector and NonOwningPtrVector
    implement their own copy methods that behave accordingly.
    
    OwningPtrVector also deletes the vector elements on destruction.

--- include/containers.hh
+++ include/containers.hh
@@ -18,49 +18,55 @@ namespace librdag {
 
 /*
  * A vector that holds pointers. Null pointers are not allowed.
- *
- * Data that a NonOwningPtrVector points to is not owned by it.
  */
 template<typename T>
-class NonOwningPtrVector
+class PtrVector
 {
   public:
-    NonOwningPtrVector();
-    ~NonOwningPtrVector();
+    PtrVector();
+    virtual ~PtrVector();
     typedef typename vector<T const *>::const_iterator citerator;
     void push_back(T const * arg);
     size_t size() const;
     citerator begin() const;
     citerator end() const;
     const T* operator[](size_t n) const;
-    NonOwningPtrVector* copy() const;
-  protected:
-    void emptyVector();
+    virtual PtrVector* copy() const = 0;
   private:
     vector<T const *>* _vector;
     void _check_arg(T const * arg);
 };
 
 /*
- * A vector that holds pointers. Null pointers are not allowed.
- *
- * Data that a PtrVector points to is owned by it. Classes held in a PtrVector
- * must define a method copy() that returns a pointer to a copy of itself.
+ * A vector that holds pointers that point to data that it does not own.
  */
 template<typename T>
-class PtrVector: public NonOwningPtrVector<T>
+class NonOwningPtrVector: public PtrVector<T>
 {
   public:
-    ~PtrVector();
-    PtrVector* copy() const;
+    virtual NonOwningPtrVector* copy() const override;
+};
+
+/*
+ * A vector that holds pointers that point to data that it owns.
+ *
+ * Classes held in an OwningPtrVector must define a method copy() that returns
+ * a pointer to a copy of itself.
+ */
+template<typename T>
+class OwningPtrVector: public PtrVector<T>
+{
+  public:
+    virtual ~OwningPtrVector() override;
+    virtual OwningPtrVector* copy() const override;
 };
 
 class OGNumeric;
 
 extern template class NonOwningPtrVector<int>;
 extern template class NonOwningPtrVector<OGNumeric>;
-extern template class PtrVector<int>;
-extern template class PtrVector<OGNumeric>;
+extern template class OwningPtrVector<int>;
+extern template class OwningPtrVector<OGNumeric>;
 
 } // namespace librdag
 
