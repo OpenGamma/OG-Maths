2bb2bdd07eea35dc8c0cf4e5d9da6d6913f7dc33
Stuart Archibald <stuart@opengamma.com>
2013-10-17 11:57:48 +0100

    [MAT-235] rebase on current master, fix ups

--- include/jdispatch.hh
+++ include/jdispatch.hh
@@ -13,11 +13,16 @@
 #include "register.hh"
 #include "warningmacros.h"
 #include "exceptions.hh"
+#include "jvmmanager.hh"
 
 namespace convert {
 
 using namespace librdag;  
-  
+
+jobjectArray convertCreal16ArrOfArr2JDoubleArrOfArr(JNIEnv * env, real16 ** inputData, int rows, int cols);
+jobjectArray extractRealPartOfCcomplex16ArrOfArr2JDoubleArrOfArr(JNIEnv * env, complex16 ** inputData, int rows, int cols);
+jobjectArray extractImagPartOfCcomplex16ArrOfArr2JDoubleArrOfArr(JNIEnv * env, complex16 ** inputData, int rows, int cols);
+
 class DispatchToReal16ArrayOfArrays: public librdag::Visitor
 {
   public:
@@ -73,6 +78,30 @@ class DispatchToComplex16ArrayOfArrays: public librdag::Visitor
     int cols;    
 };
   
+class DispatchToOGTerminal: public librdag::Visitor
+{
+  public:
+    DispatchToOGTerminal(JNIEnv* env);
+    virtual ~DispatchToOGTerminal();
+    virtual void visit(librdag::OGExpr const *thing);
+    virtual void visit(librdag::OGScalar<real16> const *thing);
+    virtual void visit(librdag::OGScalar<complex16> const *thing);
+    virtual void visit(librdag::OGScalar<int> const *thing);
+    virtual void visit(librdag::OGMatrix<real16> const *thing);
+    virtual void visit(librdag::OGMatrix<complex16> const *thing);
+    virtual void visit(librdag::OGDiagonalMatrix<real16> const *thing);
+    virtual void visit(librdag::OGDiagonalMatrix<complex16> const *thing);
+    virtual void visit(librdag::OGSparseMatrix<real16> const *thing);
+    virtual void visit(librdag::OGSparseMatrix<complex16> const *thing);
+    jobject getObject();
+  private:
+    void setObject(jobject obj);
+    jobject _obj = nullptr;
+    JNIEnv* _env;
+};
+
+
+
 } // namespace convert
 
 #endif // _JDISPATCH_HH
