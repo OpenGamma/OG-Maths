1425d9fa84d28dc9606e7b1844bd027e11798b00
Stuart Archibald <stuart@opengamma.com>
2013-10-18 11:59:32 +0100

    [MAT-263] moved standard types out of register so they can be called from elsewhere in testing.

---
+++ src/librdag/numerictypes.cc
@@ -0,0 +1,77 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+#include "numerictypes.hh"
+
+using namespace std;
+
+namespace librdag {
+
+real16 getNaN()
+{
+  union {
+    real16 d;
+    unsigned long long int i;
+  } nanval;
+  nanval.i = 0x7FF1010101010101;
+  return nanval.d;
+}
+
+complex16 getComplexNaN()
+{
+  union {
+    real16 d;
+    unsigned long long int i;
+  } nanval1, nanval2;
+  nanval1.i = 0x7FFDEAD101010101;
+  nanval2.i = 0x7FFDEAD202020202;
+  return complex16(nanval1.d, nanval2.d);
+}
+
+real16 getPosInf()
+{
+  union {
+    real16 d;
+    unsigned long long int i;
+  } val;
+  val.i = 0x7FF0000000000000;
+  return val.d;
+}
+
+real16 getNegInf()
+{
+  union {
+    real16 d;
+    unsigned long long int i;
+  } val;
+  val.i = 0xFFF0000000000000;
+  return val.d;
+}
+
+complex16 getPosPosComplexInf()
+{
+ complex16 val = {getPosInf(), getPosInf()};
+ return val;
+}
+
+complex16 getPosNegComplexInf()
+{
+ complex16 val = {getPosInf(), getNegInf()};
+ return val;
+}
+
+complex16 getNegPosComplexInf()
+{
+ complex16 val = {getNegInf(), getPosInf()};
+ return val;
+}
+
+complex16 getNegNegComplexInf()
+{
+   complex16 val = {getNegInf(), getNegInf()};
+ return val;
+}
+
+}
