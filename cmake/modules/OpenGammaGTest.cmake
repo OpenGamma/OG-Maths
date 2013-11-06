f742d259f427ae9c13945df36f81539d1646b540
Graham Markall <graham@opengamma.com>
2013-11-06 15:35:51 +0000

    [MAT-284] More PR fixup.

--- cmake/modules/OpenGammaGTest.cmake
+++ cmake/modules/OpenGammaGTest.cmake
@@ -13,6 +13,10 @@ set(GTEST_REPORT ${GTEST_XML_DIR}/report.html)
 set(GTEST_XSL ${CMAKE_SOURCE_DIR}/external/ant/junit-noframes.xsl)
 file(MAKE_DIRECTORY ${GTEST_XML_DIR})
 
+# Executes the target as a test once, and then once again with valgrind
+# Appropriate flags are added so that GTest outputs JUnit XML for later
+# processing. The target is appended to the list of tests that is used in the
+# add_gtest_report macro.
 macro(add_gtest TESTNAME)
   get_target_property(TEST_LOC ${TESTNAME} LOCATION)
   add_test(${TESTNAME} ${TEST_LOC} --gtest_output=xml:${CMAKE_BINARY_DIR}/gtest-output/${TESTNAME}.xml)
@@ -23,9 +27,15 @@ macro(add_gtest TESTNAME)
   set(gtests ${gtests} CACHE INTERNAL "GTest tests to run")
 endmacro()
 
+# Builds a test executable that uses Gtest from the specified SOURCES and links
+# it to any additional LINK_LIBRARIES. LINK_LIBRARIES does not need to include
+# the GTest libraries - these are added automatically. The sources are also
+# compiled with the specified COMPILE_DEFINITIONS.
 macro(add_multitarget_gtest TESTNAME)
-  cmake_parse_arguments(MTGTEST "" "" "SOURCES;LINK_LIBRARIES;COMPILE_DEFINITIONS" ${ARGN})
-  foreach(TARGET ${TARGET_TYPES})
+  cmake_parse_arguments(MTGTEST "" ""
+                        "SOURCES;LINK_LIBRARIES;COMPILE_DEFINITIONS;TARGETS"
+                        ${ARGN})
+  foreach(TARGET ${MTGTEST_TARGETS})
     if(SUPPORT_${TARGET})
       set(TEST ${TESTNAME}_${TARGET})
       add_executable(${TEST} ${MTGTEST_SOURCES})
@@ -42,7 +52,9 @@ macro(add_multitarget_gtest TESTNAME)
   endforeach()
 endmacro()
 
-# This must be run after you have added all your gtest tests
+# This must be run after you have added all your gtest tests. The output XMLs
+# are aggregated and turned into a report when the testresport target is run,
+# but only gtests that were already known about can be added.
 macro(add_gtest_report)
   foreach(gtest ${gtests})
     list(APPEND GTEST_XMLS ${GTEST_XML_DIR}/${gtest}.xml)
