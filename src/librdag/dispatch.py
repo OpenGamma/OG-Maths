decc7e4d2ad3d897eb9e441a6c8bdd82e5ef395a
Graham Markall <graham@opengamma.com>
2013-11-07 10:15:03 +0000

    [MAT-278] Don't delete non-converted arguments.

--- src/librdag/dispatch.py
+++ src/librdag/dispatch.py
@@ -19,7 +19,7 @@ from templates import dispatch_header, dispatcher_class, dispatcher_forward_decl
                       dispatchbinaryop_eval, dispatchbinaryop_eval_case_arg0, \
                       dispatchbinaryop_eval_case_arg1, dispatchbinaryop_terminal_method, \
                       dispatchop_class, dispatchbinaryop_conv_arg, dispatchop_methods, \
-                      dispatchbinaryop_noconv_arg
+                      dispatchbinaryop_noconv_arg, dispatchbinaryop_deletion
 
 class Dispatcher(object):
     """Generates the Dispatcher class definition and method implementations"""
@@ -161,21 +161,26 @@ class DispatchBinaryOp(object):
             if t0.name == 'OGComplexMatrix':
                 d = { 'argno': '0', 'nodetype': 'OGComplexMatrix' }
                 conv0 = dispatchbinaryop_noconv_arg % d
+                arg0_deletions = ''
             else:
                 d = { 'argno': '0', 'typetoconvertto': 'OGComplexMatrix' }
                 conv0 = dispatchbinaryop_conv_arg %d
+                arg0_deletions = dispatchbinaryop_deletion % { 'argno': '0' }
             eval_arg1_cases = ''
             for t1 in self._terminals:
+                deletions = arg0_deletions
                 if t1.name == 'OGComplexMatrix':
                     d = { 'argno': '1', 'nodetype': 'OGComplexMatrix' }
                     conv1 = dispatchbinaryop_noconv_arg % d
                 else:
                     d = { 'argno': '1', 'typetoconvertto': 'OGComplexMatrix' }
                     conv1 = dispatchbinaryop_conv_arg %d
+                    deletions += dispatchbinaryop_deletion % { 'argno': '1' }
                 d = { 'node0type': t0.name, 'node0enumtype': t0.enumname,
                       'node1type': t1.name, 'node1enumtype': t1.enumname,
                       'conv0': conv0,
-                      'conv1': conv1 }
+                      'conv1': conv1,
+                      'deletions': deletions }
                       # FIXME: Those types are the dumbest possible choices.
                 eval_arg1_cases += dispatchbinaryop_eval_case_arg1 % d
                 if t0.name not in self._backstop_terminals or t0 != t1:
