49c70631757f34af8b294ae69156cf1f32b3040c
Graham Markall <graham@opengamma.com>
2013-11-07 10:45:43 +0000

    [MAT-278] Convert to Real or Complex matrix depending on arguments.

--- src/librdag/dispatch.py
+++ src/librdag/dispatch.py
@@ -4,6 +4,7 @@
 # Please see distribution for license.
 #
 
+import re
 from templates import dispatch_header, dispatcher_class, dispatcher_forward_decl, \
                       dispatcher_dispatch_prototype, dispatcher_private_member, \
                       dispatchunaryop_class, dispatchunaryop_run, \
@@ -157,23 +158,30 @@ class DispatchBinaryOp(object):
     def method_definitions(self):
         eval_cases = ''
         terminal_methods = ''
+        complex_re = re.compile('.*Complex.*')
         for t0 in self._terminals:
-            if t0.name == 'OGComplexMatrix':
-                d = { 'argno': '0', 'nodetype': 'OGComplexMatrix' }
-                conv0 = dispatchbinaryop_noconv_arg % d
-                arg0_deletions = ''
-            else:
-                d = { 'argno': '0', 'typetoconvertto': 'OGComplexMatrix' }
-                conv0 = dispatchbinaryop_conv_arg %d
-                arg0_deletions = dispatchbinaryop_deletion % { 'argno': '0' }
             eval_arg1_cases = ''
             for t1 in self._terminals:
-                deletions = arg0_deletions
-                if t1.name == 'OGComplexMatrix':
-                    d = { 'argno': '1', 'nodetype': 'OGComplexMatrix' }
+                # Figure out which type we need to convert to
+                if complex_re.match(t0.name) or complex_re.match(t1.name):
+                    type_to_convert_to = 'OGComplexMatrix'
+                else:
+                    type_to_convert_to = 'OGRealMatrix'
+                # Check if we need to convert arg0 at all and act accordingly
+                if t0.name == type_to_convert_to:
+                    d = { 'argno': '0', 'nodetype': type_to_convert_to }
+                    conv0 = dispatchbinaryop_noconv_arg % d
+                    deletions = ''
+                else:
+                    d = { 'argno': '0', 'typetoconvertto': type_to_convert_to }
+                    conv0 = dispatchbinaryop_conv_arg %d
+                    deletions = dispatchbinaryop_deletion % { 'argno': '0' }
+                # Check if we need to convert arg1 at all and act accordingly
+                if t1.name == type_to_convert_to:
+                    d = { 'argno': '1', 'nodetype': type_to_convert_to }
                     conv1 = dispatchbinaryop_noconv_arg % d
                 else:
-                    d = { 'argno': '1', 'typetoconvertto': 'OGComplexMatrix' }
+                    d = { 'argno': '1', 'typetoconvertto': type_to_convert_to }
                     conv1 = dispatchbinaryop_conv_arg %d
                     deletions += dispatchbinaryop_deletion % { 'argno': '1' }
                 d = { 'node0type': t0.name, 'node0enumtype': t0.enumname,
@@ -181,7 +189,6 @@ class DispatchBinaryOp(object):
                       'conv0': conv0,
                       'conv1': conv1,
                       'deletions': deletions }
-                      # FIXME: Those types are the dumbest possible choices.
                 eval_arg1_cases += dispatchbinaryop_eval_case_arg1 % d
                 if t0.name not in self._backstop_terminals or t0 != t1:
                     terminal_methods += dispatchbinaryop_terminal_method %d
