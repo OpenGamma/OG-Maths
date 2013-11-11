58bbb9e3ebbb5632831a525573075ec50de25604
Graham Markall <graham@opengamma.com>
2013-11-11 09:23:42 +0000

    [MAT-290] Rename Function to Expression and make it a Numeric.

--- src/librdag/exprtree.py
+++ src/librdag/exprtree.py
@@ -8,6 +8,9 @@ import re
 from util import camel2underscore
 
 class Numeric(object):
+    """Numeric is the base class for all expression tree nodes.
+
+    All expression tree nodes have a typename and and enum name."""
     def __init__(self, typename, enumname):
         self._typename = typename
         self._enumname = enumname
@@ -21,9 +24,11 @@ class Numeric(object):
         return self._enumname
 
     def __eq__(self, other):
+        """Two Numerics are equal if they have the same type."""
         return self.typename == other.typename
 
 class Terminal(Numeric):
+    """A terminal is a numeric. It also has a datatype and a storage type."""
     def __init__(self, datatype, storagetype):
         typename = "OG%s%s" % (datatype, storagetype)
         enumname = '%s_%s_ENUM' % (datatype.upper(), camel2underscore(storagetype).upper())
@@ -38,3 +43,8 @@ class Terminal(Numeric):
     @property
     def storagetype(self):
         return self._storagetype
+
+class Expression(Numeric):
+    """A Function is used to generate the code for a single node."""
+    def __init__(self, typename, enumname):
+        super(Expression, self).__init__(typename, enumname)
