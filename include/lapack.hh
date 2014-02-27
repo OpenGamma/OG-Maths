319aec2768f68cc43adb277a599fd81dc716232a
Stuart Archibald <stuart@opengamma.com>
2014-02-27 12:10:56 +0000

    [MAT-298] add matrix dnorm2()

--- include/lapack.hh
+++ include/lapack.hh
@@ -9,7 +9,10 @@
 
 #define F77FUNC(__FUNC) __FUNC##_
 
-// these lurk in global namespace FTW F77
+// these lurk in global namespace
 extern "C" real16 F77FUNC(dnrm2)(int * N, real16 * X, int * INCX);
+
+extern "C" real16 F77FUNC(dgesvd)(char * JOBU, char * JOBVT, int * M, int * N, real16 * A, int * LDA, real16 * S, real16 * U, int * LDU, real16 * VT, int * LDVT, real16 * WORK, int * LWORK, int * INFO);
+
 
 #endif // _LAPACK_HH
