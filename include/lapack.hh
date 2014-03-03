3866f7b4e7fe4848bbb1043fb26f89427a0749fd
Stuart Archibald <stuart@opengamma.com>
2014-03-03 15:34:20 +0000

    [MAT-298] Add complex space norm function and tests.

--- include/lapack.hh
+++ include/lapack.hh
@@ -10,9 +10,14 @@
 #define F77FUNC(__FUNC) __FUNC##_
 
 // these lurk in global namespace
-extern "C" real16 F77FUNC(dnrm2)(int * N, real16 * X, int * INCX);
 
-extern "C" real16 F77FUNC(dgesvd)(char * JOBU, char * JOBVT, int * M, int * N, real16 * A, int * LDA, real16 * S, real16 * U, int * LDU, real16 * VT, int * LDVT, real16 * WORK, int * LWORK, int * INFO);
+// Standard NORM2 implementations.
+extern "C" real16 F77FUNC(dnrm2)(int * N, real16 * X, int * INCX);
+extern "C" real16 F77FUNC(dznrm2)(int * N, complex16 * X, int * INCX);
+
+// Standard SVD implementations.
+extern "C" void F77FUNC(dgesvd)(char * JOBU, char * JOBVT, int * M, int * N, real16 * A, int * LDA, real16 * S, real16 * U, int * LDU, real16 * VT, int * LDVT, real16 * WORK, int * LWORK, int * INFO);
+extern "C" void F77FUNC(zgesvd)(char * JOBU, char * JOBVT, int * M, int * N, complex16 * A, int * LDA, real16 * S, complex16 * U, int * LDU, complex16 * VT, int * LDVT, complex16 * WORK, int * LWORK, real16 * RWORK, int * INFO);
 
 
 #endif // _LAPACK_HH
