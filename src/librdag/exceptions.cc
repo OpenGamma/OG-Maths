2ca9eaa9b9bd662315a3797a136210f4ae537c08
Graham Markall <graham@opengamma.com>
2014-03-10 17:04:15 +0000

    [MAT-300] Add exceptions with stack trace info
    This required exceptions to be split into a separate namespace,
    dogma_exceptions, since it contains code common to librdag and
    convert.
    
    The exceptions use GNU execinfo to construct a backtrace and
    record this in BacktraceElement objects, one for each frame.
    
    The existing rdag_error and convert_error classes are now direct
    subclasses of dogma_error.
    
    Some testing is added to check the exception classes where
    possible. Since the stack can vary from one environment to the
    next, only the topmost frames are examined for their function
    names (addresses and filenames can vary from run to run). In the
    case of assignment and copy, comparisons are made between the
    original and new object to avoid this inconsistency between
    machines/runs being a problem.

---
+++ src/librdag/exceptions.cc
@@ -0,0 +1,235 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+
+#include <sstream>
+#include <iostream>
+#include <string.h>
+#include <regex>
+
+#ifndef __MINGW32__
+#include <execinfo.h>
+#include <cxxabi.h>
+#endif // __MINGW32__
+
+#include "exceptions.hh"
+#include "debug.h"
+
+using namespace std;
+
+namespace dogma_exceptions {
+
+#ifdef __APPLE__
+vector<string> tokenize_backtrace(const string &backtrace_string)
+{
+  istringstream s(backtrace_string);
+  vector<string> trace{istream_iterator<string>{s}, istream_iterator<string>{}};
+  if (trace.size() != 6)
+  {
+    // Parse fail. Return empty vector.
+    return vector<string>();
+  }
+  else
+  {
+    // Parse is OK.
+    return trace;
+  }
+}
+#endif // __APPLE__
+
+string get_file(const string& backtrace_string)
+{
+#ifdef __APPLE__
+  vector<string> tokens = tokenize_backtrace(backtrace_string);
+  if (tokens.size() != 0)
+  {
+    return tokens[1];
+  }
+  else
+  {
+    return string("");
+  }
+#else
+  size_t bracket = backtrace_string.find("(");
+  if (bracket == string::npos)
+  {
+    return "";
+  }
+  return backtrace_string.substr(0, bracket);
+#endif // __APPLE__
+}
+
+string get_fname(const string& backtrace_string)
+{
+  string fname;
+
+#ifdef __APPLE__
+  vector<string> tokens = tokenize_backtrace(backtrace_string);
+  if (tokens.size() != 0)
+  {
+    fname = tokens[3];
+  }
+  else
+  {
+    fname = "";
+  }
+#else
+  size_t begin = backtrace_string.find("(");
+  if (begin == string::npos)
+  {
+    return "";
+  }
+
+  // Step over the bracket
+  begin += 1;
+  size_t end = backtrace_string.find(")");
+  if (end == string::npos)
+  {
+    return "";
+  }
+
+  fname = backtrace_string.substr(begin, end-begin);
+
+  // Get rid of the offset if there is one
+  size_t offset = fname.find("+");
+  if (offset != string::npos)
+  {
+    fname = fname.substr(0, offset);
+  }
+#endif // __APPLE__
+
+  return fname;
+}
+
+const string demangle_fname(const string& fname, int* status)
+{
+  string ret = fname;
+#ifndef __MINGW32__
+  char* demangled = abi::__cxa_demangle(fname.c_str(), nullptr, 0, status);
+  switch (*status)
+  {
+  case 0:
+    ret = string(demangled); // Success
+    free(demangled);
+    break;
+  case 1:
+    DEBUG_PRINT("DEMANGLE_MALLOC_FAILED");
+    break;
+  case 2:
+    DEBUG_PRINT("DEMANGLE_INVALID_MANGLED_NAME");
+    break;
+  case 3:
+    DEBUG_PRINT("DEMANGLE_INVALID_ARGUMENT");
+    break;
+  default:
+    DEBUG_PRINT("DEMANGLE_UNKNOWN_ERROR");
+    break;
+  }
+#else
+  *status = 0;
+#endif // __MINGW32__
+  return ret;
+}
+
+/**
+ * BacktraceElement
+ */
+
+BacktraceElement::BacktraceElement()
+{
+  _address = nullptr;
+  _function = string("<function>");
+  _object_file = string("<object file>");
+}
+
+BacktraceElement::BacktraceElement(const BacktraceElement& other)
+{
+  _address = other.getAddress();
+  _function = other.getFunction();
+  _object_file = other.getObjectFile();
+}
+
+BacktraceElement::BacktraceElement(const string& backtrace_symbol, const void* address)
+{
+  _address = address;
+  _object_file = get_file(backtrace_symbol);
+  int status = 0;
+  _function = demangle_fname(get_fname(backtrace_symbol), &status);
+  _object_file = get_file(backtrace_symbol);
+}
+
+const void*
+BacktraceElement::getAddress() const
+{
+  return _address;
+}
+
+const string
+BacktraceElement::getObjectFile() const
+{
+  return _object_file;
+}
+
+const string
+BacktraceElement::getFunction() const
+{
+  return _function;
+}
+
+/**
+ * dogma_error
+ */
+
+dogma_error::dogma_error(const string& what): runtime_error(what)
+{
+#ifndef __MINGW32__
+  void *frameBuf[FRAMEBUF_SIZE];
+  char **strings;
+  _traceSize = backtrace(frameBuf, FRAMEBUF_SIZE);
+  strings = backtrace_symbols(frameBuf, _traceSize);
+
+  _backtrace = new BacktraceElement[_traceSize];
+
+  for (size_t i = 0; i < _traceSize; i++)
+  {
+    _backtrace[i] = BacktraceElement(strings[i], frameBuf[i]);
+  }
+
+  free(strings);
+#endif // __MINGW32__
+}
+
+dogma_error::dogma_error(const dogma_error& other): runtime_error::runtime_error(other)
+{
+#ifndef __MINGW32__
+  _traceSize = other.getTraceSize();
+  _backtrace = new BacktraceElement[_traceSize];
+  for (size_t i = 0; i < _traceSize; i++)
+  {
+    _backtrace[i] = BacktraceElement(other._backtrace[i]);
+  }
+#endif // __MINGW32__
+}
+
+dogma_error::~dogma_error()
+{
+#ifndef __MINGW32__
+  delete[] _backtrace;
+#endif // __MINGW32__
+}
+
+const BacktraceElement*
+dogma_error::getBacktrace() const
+{
+  return _backtrace;
+}
+
+size_t
+dogma_error::getTraceSize() const
+{
+  return _traceSize;
+}
+
+} // end namespace dogma_exceptions
